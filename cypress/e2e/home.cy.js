877ed3177d2fa28bb5f4616a17f78b784160798b
maikolcw <whysoraccoony@gmail.com>
2023-07-07 20:42:22 -0700

    added before each and updated check features homepage test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,25 +1,38 @@
 describe("home page", () => {
-  it("the h1 contains the correct text", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000")
+  })
+
+  it("the h1 contains the correct text", () => {
     cy.get("h1")
-    .should('exist')
-    .contains("Testing Next.js Applications with Cypress")
+      .should('exist')
+      .contains("Testing Next.js Applications with Cypress")
   })
 
   // better practice is to find element with data-test attribute
   it("the h1 contains the correct text", () => {
-    cy.visit("http://localhost:3000")
     cy.get(`[data-test="hero-heading"]`)
-    .should('exist')
-    .contains("Testing Next.js Applications with Cypress")
+      .should('exist')
+      .contains("Testing Next.js Applications with Cypress")
   })
 
   // Updating tests reruns all tests, with .only, only this test will run
-  it.only("the features on the homepage are correct", () => {
-    cy.visit("http://localhost:3000")
+  // it.only("the features on the homepage are correct", () => {
+  //   cy.get("dt")
+  //     .eq(0)
+  //     .contains("4 Courses")
+  // })
+
+  it("the features on the homepage are correct", () => {
     cy.get("dt")
-    .eq(0)
-    .contains("4 courses")
+      .eq(0)
+      .contains("4 Courses")
+    cy.get("dt")
+      .eq(1)
+      .contains("25+ Lessons")
+    cy.get("dt")
+      .eq(2)
+      .contains("Free and Open Source")
   })
-  
+
 })
