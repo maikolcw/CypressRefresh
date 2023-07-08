01938a27b9cf2886e3d2caa22e5a082880a0acc7
maikolcw <whysoraccoony@gmail.com>
2023-07-07 20:21:29 -0700

    updated first test with cypress better practice with data-test attribute

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -5,4 +5,12 @@ describe("home page", () => {
     .should('exist')
     .contains("Testing Next.js Applications with Cypress")
   })
+
+  // better practice is to find element with data-test attribute
+  it("the h1 contains the correct text", () => {
+    cy.visit("http://localhost:3000")
+    cy.get(`[data-test="hero-heading"]`)
+    .should('exist')
+    .contains("Testing Next.js Applications with Cypress")
+  })
 })
