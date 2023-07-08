f680ce94e1fb232b6b2a6a75fc7e8f15d50ce1f3
maikolcw <whysoraccoony@gmail.com>
2023-07-07 21:43:32 -0700

    completed user journey first course test

--- cypress/e2e/user-journey.cy.js
+++ cypress/e2e/user-journey.cy.js
@@ -22,6 +22,24 @@ describe("User Journey", () => {
             .should("eq",
                 "/testing-your-first-application/installing-cypress-and-writing-our-first-test"
             )
+
+        cy.getByData("challenge-answer-0")
+            .click()
+        cy.getByData("next-lesson-button")
+            .should("exist")
+            .click()
+        cy.location("pathname")
+            .should(
+                "eq",
+                "/testing-your-first-application/setting-up-data-before-each-test"
+            )
+        cy.getByData("challenge-answer-0")
+            .click()
+        cy.getByData("next-lesson-button")
+            .should("exist")
+            .click()
+        cy.location("pathname")
+            .should("equal", "/")
     })
 })
 
